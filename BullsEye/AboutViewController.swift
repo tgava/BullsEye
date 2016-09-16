6f10b25df4010b060c1d1e1cefd990a2454c50de
Tichafa Gava <tgava@Tichafas-MacBook-Pro.local>
2016-09-16 14:44:35 +0100

    Polishing stage with about screen

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Tichafa Gava on 16/09/2016.
+//  Copyright © 2016 Tichafa Gava - Ketts. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
