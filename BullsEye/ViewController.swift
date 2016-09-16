25c959797206d6bf92bf3290ec80702798efc49d
Tichafa Gava <tgava@Tichafas-MacBook-Pro.local>
2016-09-16 12:35:31 +0100

    Rounds and score updates

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     // declare variable for storing the value of the slider
+    // Specifying INT is optional since SWIFT can infer the type
     var currentValue: Int = 0
     var targetValue: Int = 0
+    var score = 0
+    var round = 0
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
 
     override func viewDidLoad() {
@@ -37,7 +42,32 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        
+        //calculate the difference between slider value and target value
+        // ALGORITHM 3
+        let difference = abs(targetValue - currentValue)
+        
+        /* ALGORITHM 2
+ 
+        var difference = currentValue - targetValue
+        if difference < 0 {
+            difference = difference * -1
+        }
+         */
+        /* ALGORITHM 1 
+         var difference: Int
+        if currentValue > targetValue {
+            difference = currentValue - targetValue
+        } else if targetValue > currentValue {
+            difference = targetValue - currentValue
+        } else {
+            difference = 0
+        }
+        */
+        let points = 100 - difference
+        score += points
+        
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\nThe difference is: \(difference)" + "\nYou scored : \(points)"
         let alert = UIAlertController(title: "Hello,friend", message: message, preferredStyle: .alert)
         let action =  UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
@@ -53,6 +83,7 @@ class ViewController: UIViewController {
         //print("The value of the slider is now: \(currentValue)")
     }
     func startNewRound() {
+        round += 1
     targetValue = 1 + Int(arc4random_uniform(100))
     currentValue = 50
     slider.value = Float(currentValue)
@@ -60,6 +91,8 @@ class ViewController: UIViewController {
     
     func updateLabels() {
     targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 }
 
